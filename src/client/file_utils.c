50c8b89c7f97831b071653593ed644c04bbaeded
Dinesh Pandey <dinesh.pandey@gmail.com>
2016-11-19 14:28:04 +0530

    A function to calculate hash

--- src/client/file_utils.c
+++ src/client/file_utils.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <openssl/sha.h>
 #include <errno.h>
 #include "file_utils.h"
 
@@ -33,3 +35,41 @@ create_cache_dir_if_missing(char *cache_dir)
     return OK;
 }
 
+tStatus
+calculate_sha1_hash(char *infile)
+{
+    tStatus                           ret;
+    struct                      stat fbuf;
+    unsigned char           *file_content;
+    FILE                               *f;
+    unsigned char hash[SHA_DIGEST_LENGTH]; // == 20
+
+
+    if (0 > stat(infile, &fbuf))
+    {
+        fprintf(stderr, "stat failed %d\n", errno);
+        exit(1);
+    }
+
+    file_content = (unsigned char *)malloc(fbuf.st_size);
+    if (file_content == NULL)
+    {
+        fprintf(stderr, "malloc failed %d\n", errno);
+        exit(1);
+    }
+
+    f = fopen(infile, "rb");
+    if (f == NULL)
+    {
+        fprintf(stderr, "fopen failed %d\n", errno);
+        exit(1);
+    }
+
+    ret = fread(file_content, fbuf.st_size, 1, f);
+    if (1 == ret)
+    {
+        SHA1(file_content, fbuf.st_size , hash);
+    }
+
+    return OK;
+}
