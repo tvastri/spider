e9b0ecea281dbb39b6431a28d7c8bf1da8893966
Dinesh Pandey <dinesh.pandey@gmail.com>
2016-11-20 22:10:56 +0530

    Enhanced server to implement get

--- src/server/server.go
+++ src/server/server.go
@@ -1,27 +1,58 @@
 package main
 
 import (
-        "fmt"
+        "os"
         "io"
+        "bufio"
         "net/http"
 )
 
 func get(w http.ResponseWriter, r *http.Request) {
+    var ok error
+
+    r.ParseForm()
+
+    _,ok = os.Lstat(r.Form.Get("file"))
+    if (nil != ok) {
+        http.Error(w, "Not Found", 404)
+        return
+    }
+
+    file, err := os.Open(r.Form.Get("file"))
+    if (err != nil) {
+        http.Error(w, "File open failed", 500)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/octet-stream")
+    _,err = io.Copy(w, bufio.NewReader(file))
+    if (err != nil) {
+        http.Error(w, "File write failed", 500)
+        return
+    }
 }
 
 func put(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
 }
 
 func stat(w http.ResponseWriter, r *http.Request) {
-        r.ParseForm()
-        fmt.Printf("file=%s", r.Form.Get("file"))
-        io.WriteString(w, "100 FOUND")
+    var ok error
+
+    r.ParseForm()
+    _,ok = os.Lstat(r.Form.Get("file"))
+    if (nil == ok) {
+        io.WriteString(w, "200 OK")
+    } else {
+        http.Error(w, "Not Found", 404)
+        return
+    }
 }
 
 func main() {
-        http.HandleFunc("/get", get)
-        http.HandleFunc("/put", put)
-        http.HandleFunc("/stat", stat)
+        http.HandleFunc("/get/", get);
+        http.HandleFunc("/put/", put)
+        http.HandleFunc("/stat/", stat)
         http.ListenAndServe(":8000", nil)
 }
 
