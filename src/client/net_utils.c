69622b2d37a5078ab4fb61056c220fd3c73a92e5
Dinesh Pandey <dinesh.pandey@gmail.com>
2016-11-22 13:16:01 +0530

    Download the server config into memory

--- src/client/net_utils.c
+++ src/client/net_utils.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <curl/curl.h>
 #include "global.h"
+#include "debug.h"
+
+typedef struct
+{
+    int      size;
+    char    *data;
+} tServerConfigData;
 
 tBoolean
 hash_present_on_server()
@@ -18,16 +28,39 @@ hash_present_on_server()
     return FALSE;
 }
 
+size_t
+store_server_config(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    tServerConfigData *pServData;
+
+    pServData = (tServerConfigData *)userp;
+
+    pServData->data = (char *)realloc(pServData->data, pServData->size + size*nmemb);
+    if (NULL == pServData->data)
+    {
+        debug_log(LOG_CRIT, "malloc failed. errno = %s", strerror(errno));
+        return 0;
+    }
+
+    memcpy(pServData->data+pServData->size, buffer, size*nmemb); 
+    pServData->size += size*nmemb;
+
+    return size*nmemb;
+}
+
 tBoolean
 get_config_from_server(tServerConfig *t)
 {
-    CURL *curl;
-    CURLcode res;
+    CURL             *curl;
+    CURLcode           res;
+    tServerConfigData data = {0, NULL};
 
     curl = curl_easy_init();
     if (curl)
     {
         curl_easy_setopt(curl, CURLOPT_PROXY, "");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, store_server_config); 
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data); 
         curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:8000/conf/");
         res = curl_easy_perform(curl);
         if (res != CURLE_OK)
